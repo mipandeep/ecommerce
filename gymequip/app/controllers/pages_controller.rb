063e9ded82b21cb693ac358f61541d4270a850c7
Mipandeep <mipandeep@Mipandeeps-MacBook-Pro.local>
2019-03-15 16:53:20 -0500

    added pages controller to allow new pages to be added and edited.

--- gymequip/app/controllers/pages_controller.rb
+++ gymequip/app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
   def permalink
+    @page = Page.find_by(permalink: params[:permalink])
+
+    if @page.nil?
+      redirect_to root_url
+    else
+      render :show
+    end
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 end
--- gymequip/config/routes.rb
+++ gymequip/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/permalink'
+  
   get 'pages/show'
   get 'product/index'
   get 'product/show'
   resources :product ,only:[:index,:show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+
+  root to: 'product#index'
+  get '/:permalink' , to: 'pages#permalink' , as: 'permalink'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
