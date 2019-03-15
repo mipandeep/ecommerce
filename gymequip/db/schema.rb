3f0b10786eab702057ab616af96c2c88851a9482
Mipandeep <mipandeep@Mipandeeps-MacBook-Pro.local>
2019-03-15 16:23:28 -0500

    created pages model for contact and about page.

--- gymequip/db/schema.rb
+++ gymequip/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_203652) do
+ActiveRecord::Schema.define(version: 2019_03_15_212235) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2019_03_15_203652) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
