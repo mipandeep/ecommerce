817a46bd2a72751cfef545d5bbab9b265d5d7f17
Mipandeep <mipandeep@Mipandeeps-MacBook-Pro.local>
2019-03-15 15:09:43 -0500

    Added a product controller with index and show pages

---
+++ gymequip/db/migrate/20190314042157_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.string :description
+      t.references :category, foreign_key: true
+      t.references :product_status, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
